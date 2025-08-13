90af08a37a94d9d71f371a49ccba273f810b49bc
jac23-245 <j69867674@gmail.com>
2025-08-13 19:31:08 +0430

    js.js

--- course/js/js.js
+++ course/js/js.js
@@ -1,7 +1,7 @@
 /* Login Page */
 
 
-const BASE_URL = 'http://localhost/course';
+const BASE_URL = 'http://jac23-245/course';
 const API_URL = 'https://yaran-soft.com/ghafor/raziq';
 
 document.addEventListener('DOMContentLoaded', () => {
@@ -318,4 +318,5 @@ function getAlertMessage(messageKey, titleKey='', type="toast", icon="") {
         .catch(error => console.error('Error loading JSON:', error));
 
 }
+
 
